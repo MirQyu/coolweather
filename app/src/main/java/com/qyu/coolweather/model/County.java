d549e580c0cbba4042e28230393a206d80da3540
13qyu <qi.163@126.com>
2015-12-19 16:08:28 +0800

    新添数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/qyu/coolweather/model/County.java
@@ -0,0 +1,44 @@
+package com.qyu.coolweather.model;
+
+/**
+ * Created by qyu on 2015/12/19.
+ */
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
---
+++ app/src/main/java/com/qyu/coolweather/model/Province.java
@@ -0,0 +1,36 @@
+package com.qyu.coolweather.model;
+
+/**
+ * Created by qyu on 2015/12/19.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
